769a24d0aa8a84de20614e05a0a80b1b1a1b231a
rygatoni <officialryg2@gmail.com>
2022-11-21 00:50:16 -0500

    Changes by Jepoy

--- src/main/java/rygatoni/github/io/project_4/CurrentOrderController.java
+++ src/main/java/rygatoni/github/io/project_4/CurrentOrderController.java
@@ -1,4 +1,10 @@
 package rygatoni.github.io.project_4;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class CurrentOrderController {
+
+    @FXML
+    Button placeOrder, removePizza, clearOrder, mainMenu;
 }
--- src/main/java/rygatoni/github/io/project_4/NewYorkController.java
+++ src/main/java/rygatoni/github/io/project_4/NewYorkController.java
@@ -1,4 +1,29 @@
 package rygatoni.github.io.project_4;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class NewYorkController {
+
+    @FXML
+    Button mainMenu, addToOrder;
+
+    @FXML
+    ComboBox flavors, sizes;
+
+    @FXML
+    public void mainMenuPress() throws IOException {
+        Stage stage = (Stage) mainMenu.getScene().getWindow();
+        FXMLLoader mainLoader = new FXMLLoader(PizzaFactoryMain.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(mainLoader.load(), 320, 240);
+        stage.setTitle("RU Pizzeria");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
